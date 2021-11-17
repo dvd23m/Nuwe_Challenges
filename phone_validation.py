674b6467909fc089dab77af7ed14cea5d1570f65
dvd23m <68005809+dvd23m@users.noreply.github.com>
2021-11-17 16:11:58 +0100

    Add files via upload

---
+++ phone_validation.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Carga de librerias
+import re
+
+
+# In[ ]:
+
+
+def comprobar_telefono(telefono):
+    # Diccionario con los diferentes patrones
+    patterns = {'pat1':'[0-9]{3}-([0-9]{2}-){2}[0-9]{2}',
+               'pat2':'\([0-9]{3}\)[0-9]{3}-[0-9]{3}',
+               'pat3':'([0-9]{3}-){2}[0-9]{3}',
+               'pat4':'([0-9]{3}\s){2}[0-9]{3}',
+               'pat5':'[0-9]{9}',
+               'pat6':'34\s([0-9]{3}\s){2}[0-9]{3}',
+               'pat7': '\([0-9]{3}\)\s([0-9]{2}\s){2}[0-9]{2}',
+               'pat8': '[0-9]{3}\s([0-9]{2}\s){2}[0-9]{2}',
+               'pat9': '34\s[0-9]{3}\s([0-9]{2}\s){2}[0-9]{2}'}
+    # Bucle que recorre el diccionario comprueba cada patron con el telefono
+    for pat in patterns.values():
+        correcto = re.match(pat, str(telefono))
+        if correcto:
+            return True
+    return False
+
+
+# In[ ]:
+
+
+# Introducir telefono
+telefono = input('Introduce tu número de teléfono:')
+print(comprobar_telefono(telefono))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
