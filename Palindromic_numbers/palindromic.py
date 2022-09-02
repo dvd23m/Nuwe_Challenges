14b270fef3245292d21daa01ceed5efb726d3a04
David Molina Pulgarin <68005809+dvd23m@users.noreply.github.com>
2022-09-02 10:22:31 +0200

    Update palindromic.py

--- Palindromic_numbers/palindromic.py
+++ Palindromic_numbers/palindromic.py
@@ -4,4 +4,4 @@ for n1 in range(100,1000):
 		res = str(n1*n2)
 		if res==res[::-1]:
 			palindromos.append(int(res))
-print(f'El palindromo mas grande tras multiplicar 2 numeros de 3 cifras es {palindromos[-1]}')
+print(f'El palindromo mas grande tras multiplicar 2 numeros de 3 cifras es {max(palindromos)}')
